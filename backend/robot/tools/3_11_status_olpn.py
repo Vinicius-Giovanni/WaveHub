c8294ccfb5316ee3fe0ba96506c67d950cadd3f6
Vinicius-Giovanni <viniciusgiovanni2003@gmail.com>
2026-09-03 15:52:59 -0300

    feat(+Robot): Tool de extração do relatório 3.11 na plataforma IBM

--- backend/robot/tools/3_11_status_olpn.py
+++ backend/robot/tools/3_11_status_olpn.py
@@ -4,6 +4,8 @@ from pathlib import Path
 
 import os
 
+from backend.settings.elements_ibm import ELEMENTS_OLPN, FRAME
+
 class StatusOlpnIBM:
 
     async def extract(
@@ -24,9 +26,73 @@ class StatusOlpnIBM:
             - **last_date** data final que o relatório deve abordar
         """
 
+        from backend.settings.paths import TEMP_WAVEHUB
+
         await page.context.add_cookies(cookies) # <- Inserindo cookies no contexto da page
         
         for filial in list_filial:
 
             # Direcionar para IBM, especificamente para a página de download do relatório
             page.goto(os.getenv('LINK_STATUS_OLPN_IBM'))
+
+            # entrando no frame
+            frame = page.locator(
+                FRAME
+            )
+
+            # Localizando disponibilidade de tabela de filials
+            _filial = await frame.locator(ELEMENTS_OLPN['element_filial_id'])
+
+            if _filial:
+                await filial.select_option(filial) # <- definindo filial
+
+            # enviando data inicio
+            await frame.fill(
+                ELEMENTS_OLPN['element_dt_start'],
+                init_date
+            )
+
+            # enviando data final
+            await frame.fill(
+                ELEMENTS_OLPN['element_dt_end'],
+                last_date
+            )
+
+            # localizando template
+            await frame.locator(
+                ELEMENTS_OLPN['element_listbox']
+            ).wait_for(state='visible')
+
+            itens = frame.locator(
+                ELEMENTS_OLPN['element_listbox']
+            )
+
+            for i in range(await itens.count()):
+                item = itens.nth(i)
+
+                template = await item.get_attribute(
+                    ELEMENTS_OLPN['element_get_item']
+                )
+
+                if template in ELEMENTS_OLPN['list_itens']:
+
+                    is_checked = (
+                        await item.get_attribute(
+                            ELEMENTS_OLPN['element_get_checked']
+                        )
+                    ) == "true"
+
+                    if not is_checked:
+                        await item.click()
+
+            # Aguardar download de relatório
+
+            async with frame.expect_download() as download_info:
+                await frame.locator(ELEMENTS_OLPN['element_confirm']).click()
+
+            download = await download_info.value
+
+            await download.save_as(
+                str(TEMP_WAVEHUB / download.suggested_filename)
+            )
+
--- backend/settings/paths.py
+++ backend/settings/paths.py
@@ -1,7 +1,16 @@
 from __future__ import annotations
 from pathlib import Path
 
+import os
+
 ENV_PATH = Path(".env")
 
-
+PATH_PADRAO_SHARE = Path(os.getenv('PATH_PADRAO_SHARE'))
 COOKIES_FILE = Path(r'cookies.json')
+
+TEMP_PROFILE = Path(f"{PATH_PADRAO_SHARE}/Compartilhados/WaveHub/Temporaria Profile")
+
+TEMP_WAVEHUB = Path(f"{PATH_PADRAO_SHARE}/Compartilhados/WaveHub/Temporaria WaveHub") 
+
+# Camada Bronze
+
