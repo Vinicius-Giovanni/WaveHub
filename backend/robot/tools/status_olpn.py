1fa4426dd5e3bd540ed73ce3b51921e9945cb06b
Vinicius-Giovanni <viniciusgiovanni2003@gmail.com>
2026-09-09 11:14:30 -0300

    feat(backend): Extração de relatório 3.11 criado.

--- backend/robot/tools/status_olpn.py
+++ backend/robot/tools/status_olpn.py
@@ -26,48 +26,53 @@ class StatusOlpnIBM:
             - **last_date** data final que o relatório deve abordar
         """
 
-        from backend.settings.paths import TEMP_WAVEHUB
-
         await page.context.add_cookies(cookies) # <- Inserindo cookies no contexto da page
         
         for filial in list_filial:
 
             # Direcionar para IBM, especificamente para a página de download do relatório
-            page.goto(os.getenv('LINK_STATUS_OLPN_IBM'))
+            await page.goto(os.getenv('LINK_STATUS_OLPN_IBM'))
 
             # entrando no frame
-            frame = page.locator(
+            frame = page.frame_locator(
                 FRAME
             )
 
             # Localizando disponibilidade de tabela de filials
-            _filial = await frame.locator(ELEMENTS_OLPN['element_filial_id'])
+            _filial = frame.locator(ELEMENTS_OLPN['element_filial_id'])
 
             if _filial:
-                await filial.select_option(filial) # <- definindo filial
+                await _filial.select_option(filial) # <- definindo filial
 
             # enviando data inicio
-            await frame.fill(
-                ELEMENTS_OLPN['element_dt_start'],
-                init_date
-            )
+            await frame.locator(
+                ELEMENTS_OLPN['element_dt_start']
+            ).fill(init_date)
 
             # enviando data final
-            await frame.fill(
-                ELEMENTS_OLPN['element_dt_end'],
-                last_date
-            )
+            await frame.locator(
+                ELEMENTS_OLPN['element_dt_end']
+            ).fill(last_date)
 
             # localizando template
             await frame.locator(
                 ELEMENTS_OLPN['element_listbox']
             ).wait_for(state='visible')
 
-            itens = frame.locator(
+            lista = frame.locator(
                 ELEMENTS_OLPN['element_listbox']
             )
 
+            await lista.wait_for(state='visible')
+
+            itens = frame.locator(
+                ELEMENTS_OLPN['elements_listbox']
+            )
+
+            print("Quantidade de itens:", await itens.count())
+
             for i in range(await itens.count()):
+
                 item = itens.nth(i)
 
                 template = await item.get_attribute(
@@ -87,12 +92,11 @@ class StatusOlpnIBM:
 
             # Aguardar download de relatório
 
-            async with frame.expect_download() as download_info:
+            async with page.expect_download() as download_info:
                 await frame.locator(ELEMENTS_OLPN['element_confirm']).click()
 
             download = await download_info.value
 
             await download.save_as(
-                str(TEMP_WAVEHUB / download.suggested_filename)
+                str(download_dir / download.suggested_filename)
             )
-
