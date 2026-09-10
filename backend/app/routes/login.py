f680e1704c14ab836b551d862b72a73e75e9392c
thai-souza <thainacristinasouza@hotmail.com.br>
2026-09-10 13:27:13 -0300

    feat(frontend): - criação da página e estilização register - criação de arquivo de estilização global para as páginas de login e cadastro

--- backend/app/routes/login.py
+++ backend/app/routes/login.py
@@ -11,7 +11,7 @@ router = APIRouter(
 async def login_page(request: Request):
     return templates.TemplateResponse(
         request=request,
-        name="login.html",
+        name="register.html",
     )
 
 @router.post("/login")
